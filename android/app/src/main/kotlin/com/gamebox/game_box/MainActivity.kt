ed76229492d94691797e40136c1e43eb1337e471
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-03-29 21:58:41 +0100

    Project Creation
    Created the Flutter project and added the dependencies to .gitignore of Daart and Firebase

---
+++ android/app/src/main/kotlin/com/gamebox/game_box/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gamebox.game_box
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/routes/AppPages.dart
@@ -0,0 +1,23 @@
+
+import 'package:game_box/pages/LoadingPage.dart';
+import 'package:get/get.dart';
+import '../pages/LoginPage.dart';
+import '../pages/RegisterPage.dart';
+import 'AppRoutes.dart';
+
+class AppPages {
+  static final routes = [
+    GetPage(
+      name: Routes.loading,
+      page: () => const Loadingpage(),
+    ),
+    GetPage(
+        name: Routes.login,
+        page: () => const LoginPage(),
+    ),
+    GetPage(
+        name: Routes.register,
+        page: () => const RegisterPage(),
+    ),
+  ];
+}
