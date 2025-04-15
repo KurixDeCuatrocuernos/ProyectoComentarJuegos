3ed2bbe384d82f13d4e440494a4411eb98af84b4
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-04-15 15:23:20 +0200

    GameComponent edited
    Added a method to show the button to add a new comment or edit if it exists and the logic to show the commentaryComponent to add a new one or edit one.

--- lib/components/GameComponent.dart
+++ lib/components/GameComponent.dart
@@ -1,8 +1,11 @@
 import 'dart:math';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:game_box/components/CommentaryComponent.dart';
 import 'package:game_box/components/GameImage.dart';
+import 'package:game_box/repository/CommentaryRepository.dart';
 
 
 class GameComponent extends StatelessWidget {
@@ -20,16 +23,38 @@ class GameComponent extends StatelessWidget {
           ),
           backgroundColor: Colors.white,
           contentPadding: EdgeInsets.zero, // Elimina el relleno alrededor del contenido
-          content: CommentaryComponent(game: game!),
+          content: CommentaryComponent(game: game!, edit: false,),
+        );
+      },
+    );
+  }
+
+  void showEditCommentForm(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true, // Si el cuadro de diálogo debe cerrarse al tocar fuera de él
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          backgroundColor: Colors.white,
+          contentPadding: EdgeInsets.zero, // Elimina el relleno alrededor del contenido
+          content: CommentaryComponent(game: game!, edit: true,),
         );
       },
     );
   }
 
 
-  bool _isCommented() {
+  Future<bool> _isCommented() async{
   /// verificamos si el usuario ha comentado o no este juego
+    CommentaryRepository _commentRepo = CommentaryRepository();
+    User? _user = FirebaseAuth.instance.currentUser;
     bool cell = false;
+    if (_user != null){
+      cell = await _commentRepo.getIfUserHasCommentedThisGame(_user!, game);
+    }
     return cell;
   }
 
@@ -71,7 +96,7 @@ class GameComponent extends StatelessWidget {
             SizedBox(
               width: 128,
               height: 256,
-              child: GameImage(game: game!),
+              child: game?['cover'] != null ? GameImage(game: game!) : Text('Image Not Found'), /// Hay que cambiarlo por la imagen correspondiente
             ),
             SizedBox(
               width: 128,
@@ -112,7 +137,7 @@ class GameComponent extends StatelessWidget {
         SizedBox(
           width: 320,
           child: Text(
-              game?['summary'],
+              game?['summary'] != null ? game!['summary'] : 'This game has no abstract',
             style: TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 16,
@@ -130,34 +155,43 @@ class GameComponent extends StatelessWidget {
             Expanded(
                 child: SizedBox(),
             ),
-            _isCommented() == false ?
-            FloatingActionButton(
-              onPressed: () {},
-              shape: const CircleBorder(),
-              backgroundColor: Colors.black,
-              child: IconButton(
-                icon: Icon(
-                  Icons.add,
-                  size: 40,
-                ),
-                onPressed: () => showCommentForm(context),
-                color: Colors.white,
-              ),
-            )
-                :
-            FloatingActionButton(
-              onPressed: () {},
-              shape: const CircleBorder(),
-              backgroundColor: Colors.black,
-              child: IconButton(
-                icon: Icon(
-                  Icons.edit,
-                  size: 40,
-                ),
-                onPressed: () {print("EDITASTE TU COMENTARIO DEL JUEGO: "+game!['name']);},
-                color: Colors.white,
-              ),
+
+            FutureBuilder<bool>(
+              future: _isCommented(), // tu función async que devuelve Future<bool>
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  // Mientras espera el resultado del future
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text("Error al verificar comentario");
+                } else if (snapshot.hasData) {
+                  bool isCommented = snapshot.data!;
+
+                  return FloatingActionButton(
+                    onPressed: () {},
+                    shape: const CircleBorder(),
+                    backgroundColor: Colors.black,
+                    child: IconButton(
+                      icon: Icon(
+                        isCommented ? Icons.edit : Icons.add,
+                        size: 40,
+                      ),
+                      onPressed: () {
+                        if (isCommented) {
+                          showEditCommentForm(context);
+                        } else {
+                          showCommentForm(context);
+                        }
+                      },
+                      color: Colors.white,
+                    ),
+                  );
+                } else {
+                  return SizedBox(); // fallback
+                }
+              },
             ),
+
             SizedBox(
               width: 20,
             )
