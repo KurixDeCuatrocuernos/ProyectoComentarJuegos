3c3bd25e9987321153bd9d687e2a331bcd066dd5
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-04-15 15:16:24 +0200

    CommentaryComponent edited
    Added the feature to allow to use CommentaryComponent to add a new comment or edit one.

--- lib/components/CommentaryComponent.dart
+++ lib/components/CommentaryComponent.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:game_box/comments/controllers/CommentaryController.dart';
+import 'package:game_box/comments/models/CommentaryModel.dart';
 import 'package:game_box/comments/utils/CommentaryValidator.dart';
+import 'package:game_box/repository/CommentaryRepository.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
@@ -10,16 +13,49 @@ import 'package:provider/provider.dart';
 import '../games/services/IgdbApiRepository.dart';
 import '../routes/AppRoutes.dart';
 
-class CommentaryComponent extends StatelessWidget {
+class CommentaryComponent extends StatefulWidget {
   final Map<String, dynamic>? game;
-  CommentaryComponent({super.key, required this.game});
+  final bool edit;
 
+  const CommentaryComponent({super.key, required this.game, this.edit = false});
+
+  @override
+  _CommentaryComponentState createState() => _CommentaryComponentState();
+}
+
+class _CommentaryComponentState extends State<CommentaryComponent> {
   final _formKey = GlobalKey<FormState>();
+  late CommentaryController commentaryController;
+  late CommentaryValidator commentaryValidator;
+
+  @override
+  void initState() {
+    super.initState();
+    commentaryController = CommentaryController();
+    commentaryValidator = CommentaryValidator();
+
+    if (widget.edit) {
+      print("SE VA A EDITAR EL COMENTARIO");
+      _isEditing();
+    }
+  }
+
+  Future<void> _isEditing() async {
+    print("EDITANDO COMENTARIO");
+    User? user = FirebaseAuth.instance.currentUser;
+    Map<String, dynamic>? commentary = await CommentaryRepository().getCommentaryByUserAndGame(user!, widget.game!);
+
+    if (commentary != null) {
+      commentaryController.titleController.text = commentary?['title'];
+      commentaryController.commentController.text = commentary?['body'];
+      commentaryController.sliderController.value = commentary?['value'];
+    } else {
+      print("COMENTARIO RECOGIDO NO DISPONIBLE");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    CommentaryController commentaryController = CommentaryController();
-    CommentaryValidator commentaryValidator = CommentaryValidator();
     String? uid = FirebaseAuth.instance.currentUser?.uid;
 
     return Container(
@@ -27,10 +63,10 @@ class CommentaryComponent extends StatelessWidget {
       height: 600,
       decoration: BoxDecoration(
         color: Color(0xFF120B0B), // Color de fondo
-        borderRadius: BorderRadius.circular(20),  // Radio para redondear las esquinas
+        borderRadius: BorderRadius.circular(20),
         border: Border.all(
-          color: Colors.black, // Color del borde
-          width: 4, // Grosor del borde
+          color: Colors.black,
+          width: 4,
         ),
       ),
       child: Column(
@@ -39,7 +75,7 @@ class CommentaryComponent extends StatelessWidget {
             children: [
               Expanded(child: SizedBox(),),
               IconButton(
-                onPressed: () {Navigator.of(context).pop();}, /// Closes the Dialog
+                onPressed: () {Navigator.of(context).pop();},
                 icon: Icon(
                   Icons.close,
                   color: Colors.white,
@@ -61,32 +97,29 @@ class CommentaryComponent extends StatelessWidget {
             height: 400,
             child: Form(
               key: _formKey,
-              child:Column(
+              child: Column(
                 children: [
                   SizedBox(height: 10),
                   Flexible(
                     child: TextFormField(
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
+                      style: TextStyle(color: Colors.white),
                       validator: commentaryValidator.isValidCommentary,
                       controller: commentaryController.titleController,
                       decoration: const InputDecoration(
-                        hintText:'Resume your opinion about this videogame in a few words',
+                        hintText: 'Resume your opinion about this videogame in a few words',
                       ),
                     ),
                   ),
                   SizedBox(height: 10),
+                  SizedBox(),
                   Flexible(
                     child: SingleChildScrollView(
                       child: TextFormField(
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
+                        style: TextStyle(color: Colors.white),
                         validator: commentaryValidator.isValidCommentary,
                         controller: commentaryController.commentController,
                         decoration: const InputDecoration(
-                          hintText:'¿What do you think about the videogame?',
+                          hintText: '¿What do you think about the videogame?',
                         ),
                       ),
                     ),
@@ -95,17 +128,14 @@ class CommentaryComponent extends StatelessWidget {
                   Obx(() {
                     return Column(
                       children: [
-                        // Texto que muestra el valor del slider
                         Text(
-                            "${commentaryController.sliderController.value}",
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
+                          "${commentaryController.sliderController.value}",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-
-                        // Slider que usa el valor del slider de la clase CommentaryController
                         Slider(
                           value: commentaryController.sliderController.value.toDouble(),
                           min: 0,
@@ -113,31 +143,39 @@ class CommentaryComponent extends StatelessWidget {
                           divisions: 100,
                           label: '${commentaryController.sliderController.value}',
                           onChanged: (double newValue) {
-                            commentaryController.sliderController.value = newValue.toInt();  // Actualiza el valor del slider
+                            commentaryController.sliderController.value = newValue.toInt();
                           },
                         ),
-                        // Texto que muestra errores
                         Text(commentaryValidator.isValidValue(commentaryController.sliderController.value) ?? ''),
                       ],
                     );
                   }),
                   Container(
                     decoration: BoxDecoration(
-                      color: Colors.black, // Color de fondo
-                      shape: BoxShape.circle, // Para hacerlo circular
+                      color: Colors.black,
+                      shape: BoxShape.circle,
                     ),
                     child: IconButton(
                         onPressed: () async {
-                          bool cell=true;
-                          if(_formKey.currentState!.validate()) {
-                            try{
-                              commentaryController.addNewCommentary(game!, uid!, await Provider.of<IgdbApiRepository>(context, listen: false).getCover(game?['cover']));
+                          bool cell = true;
+                          if (_formKey.currentState!.validate()) {
+                            try {
+                              if (widget.edit==true){
+                                print("EDITANDO COMENTARIO");
+                                 cell = await commentaryController.editCommentary(widget.game!, uid!);
+                              } else {
+                                print("GUARDANDO NUEVO COMENTARIO");
+                                commentaryController.addNewCommentary(
+                                    widget.game!,
+                                    uid!,
+                                    await Provider.of<IgdbApiRepository>(context, listen: false).getCover(widget.game?['cover'])
+                                );
+                              }
                             } catch (error) {
-                              cell=false;
+                              cell = false;
                             }
-                            if (cell==true) {
-                              Get.offAllNamed(Routes.game, arguments: game);
-                              ///Se podría mostrar un dialog de confirmación y otro de negación
+                            if (cell == true) {
+                              Get.offAllNamed(Routes.game, arguments: widget.game);
                             }
                           } else {
                             print("Try again!");
@@ -148,7 +186,6 @@ class CommentaryComponent extends StatelessWidget {
                         icon: Icon(Icons.check)
                     ),
                   ),
-
                 ],
               ),
             ),
@@ -158,3 +195,4 @@ class CommentaryComponent extends StatelessWidget {
     );
   }
 }
+
