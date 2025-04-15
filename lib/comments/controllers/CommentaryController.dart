005b7cfc09ce525187a347d29be551d61b1d3d10
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-04-15 15:11:26 +0200

    CommentaryController edited
    Added first_release_date to game collection in database in addNewGame() method.
    Added createdAt field to comment collection in addNewCommentary() method.
    Added editCommentary() method to update a commentary from database.

--- lib/comments/controllers/CommentaryController.dart
+++ lib/comments/controllers/CommentaryController.dart
@@ -6,22 +6,25 @@ class CommentaryController extends GetxController {
   TextEditingController commentController = TextEditingController();
   TextEditingController titleController = TextEditingController();
   var sliderController = 50.obs;
+  String _collectionC = 'comments';
+  String _collectionG = 'games';
 
   addNewCommentary(Map<String, dynamic> game, String UID, URL) async{
     print("AÑADIDO EL COMENTARIO:\nTITLE: ${titleController.text}\nBODY: ${commentController.text}\nVALUE: ${sliderController.value}\nUSER: $UID\nGAME: ${game['id']}");
     final doc = await FirebaseFirestore.instance
-    .collection('games')
+    .collection(_collectionG)
     .doc(game['id'].toString())
     .get();
     print("SE VA A AÑADIR EL JUEGO A LA BASE DE DATOS");
     addNewGame(game, URL);
 
-    final commentSetted = await FirebaseFirestore.instance.collection('comments').add({
+    final commentSetted = await FirebaseFirestore.instance.collection(_collectionC).add({
       'title': titleController.text,
       'body': commentController.text,
       'value': sliderController.value,
       'userId': UID,
-      'gameID': game['id']
+      'gameId': game['id'],
+      'createdAt': FieldValue.serverTimestamp(),
     });
 
 
@@ -31,15 +34,55 @@ class CommentaryController extends GetxController {
   addNewGame(Map<String, dynamic> game, URL) async {
     print("AÑADIENDO JUEGO A LA BASE DE DATOS");
     print('COVER OBTENIDA: $URL');
+    var rawTimestamp = game['first_release_date'];
+    print("FECHA RECOGIDA: $rawTimestamp");
+    if (rawTimestamp is String){
+      rawTimestamp = int.tryParse(rawTimestamp);
+    }
+    DateTime? convertedDate;
+    if (rawTimestamp != null) {
+      convertedDate = DateTime.fromMillisecondsSinceEpoch(rawTimestamp * 1000);
+      print("FECHA CONVERTIDA: $convertedDate");
+    }
+
     final gameSetted = await FirebaseFirestore.instance
-        .collection('games').doc(game['id'].toString()).set({
+        .collection(_collectionG).doc(game['id'].toString()).set({
       'id': game['id'],
       'name': game['name'],
       'summary': game['summary'],
       'rating': game['rating'],
       'coverId': game['cover'],
-      'url': URL
+      'url': URL,
+      'first_release_date': rawTimestamp != null ? convertedDate : null,
     }, SetOptions(merge: true));
+  }
+
+  Future<bool> editCommentary(Map<String, dynamic> game, String uid) async {
+    bool cell = true;
+    print("SUBIENDO MODIFICACIONES A LA BASE DE DATOS");
+    try {
+      String commentId;
+      QuerySnapshot response = await FirebaseFirestore.instance.collection(_collectionC)
+          .where('gameId', isEqualTo: game['id'])
+          .where('userId', isEqualTo: uid)
+          .get();
+      if (response.docs.isNotEmpty){
+        commentId = response.docs[0].id;
+        await FirebaseFirestore.instance.collection(_collectionC)
+            .doc(commentId)
+            .update({
+              'title': titleController.text,
+              'body': commentController.text,
+              'value': sliderController.value,
+              'createdAt': FieldValue.serverTimestamp(),
+            })
+            .then((_) => print("COMENTARIO ACTUALIZADO"));
+      }
+    } catch (error) {
+      print("HUBO UN ERROR AL EDITAR EL COMENTARIO: $error");
+      cell = false;
+    }
+    return cell;
   }
 
 }
