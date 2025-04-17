ab2df112568f1c93085cfb0f6139578939630f4b
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-04-17 21:31:06 +0200

    ProfilePage edited
    Finished the profile page to show the ProfileComponent

---
+++ lib/pages/ProfilePage.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:game_box/components/ProfileComponent.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import '../auth/structure/controllers/AuthController.dart';
+import '../components/SearchPlaceholder.dart';
+import '../components/SearchResults.dart';
+import '../components/ToolBar.dart';
+import '../components/UserImage.dart';
+import '../components/UserName.dart';
+import '../routes/AppRoutes.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  AuthController _authController = AuthController();
+
+
+  void _signOut() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Sign Out'),
+          content: Text('¿Are you sure you want to Sign out?'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('No'),
+              onPressed: () {
+                Navigator.of(context).pop(); // It closes the dialog
+              },
+            ),
+            TextButton(
+              child: Text('Yes'),
+              onPressed: () {
+                _authController.signOut();
+                Get.offAllNamed(Routes.login);// Llamar a la función para cerrar sesión
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      bottomNavigationBar: ToolBar(),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              AppBar(
+                leading: IconButton(
+                  onPressed: () {
+                    Get.back();
+                  },
+                  color: Colors.white,
+                  icon: Icon(Icons.arrow_back),
+                ),
+                title: UserImage(size: 45,),
+                actions: [
+                  SearchPlaceholder(),
+                  if (kIsWeb) UserName(),
+                  IconButton(
+                    onPressed: _signOut,
+                    icon: Icon(Icons.logout_rounded),
+                    color: Colors.white,
+                    iconSize: 30,
+                  ),
+                ],
+                backgroundColor: Colors.black,
+              ),
+
+              Expanded(
+                  child: ProfileComponent(),
+              ),
+
+            ],
+          ),
+          SearchResults(), // Muestra la lista de resultados
+        ],
+      ),
+    );
+  }
+}
