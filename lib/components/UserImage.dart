f14170a1944cc35197cdb6bbb301eb511b91273c
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-04-17 21:26:19 +0200

    UserImage edited
    Change the component's logic to works with gravatar, also added the size property to specify the component's size in its invocation

--- lib/components/UserImage.dart
+++ lib/components/UserImage.dart
@@ -1,39 +1,95 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crypto/crypto.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:game_box/repository/UserRepository.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 
 import '../routes/AppRoutes.dart';
 
-class UserImage extends StatelessWidget {
-  const UserImage({super.key});
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
 
-  Widget _getUserImage() {
-    return Image.asset(
-      'user/userImage1.png',
-      errorBuilder: (context, error, stackTrace) {
-        print('User has not Image, charging default image');
-        if (kIsWeb) {
-          return Image.asset('google/dark/web_dark_rd_na.png');
-        } else {
-          return SvgPicture.asset('assets/google_android/dark/android_dark_rd_na.svg');
-        }
+class UserImage extends StatefulWidget {
+  final double size;
+  final bool editing;
+  const UserImage({super.key, required this.size, this.editing = false});
 
-      },
-    );
+  @override
+  _UserImageState createState() => _UserImageState();
+}
+
+class _UserImageState extends State<UserImage> {
+  String gravatarAttr = 'mp';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserImage();
+  }
+
+  Future<void> _loadUserImage() async {
+    UserRepository _userRepo = UserRepository();
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      String? image = await _userRepo.getUserImageByUid(user.uid);
+      if (image != null && mounted) {
+        setState(() {
+          gravatarAttr = image;
+        });
+      }
+    }
+  }
+
+  String _generateGravatarUrl(String email, String gravatarAttr) {
+    // print('ATRIBUTO PARA GRAVATAR: $gravatarAttr');
+    final bytes = utf8.encode(email.trim().toLowerCase());
+    final digest = md5.convert(bytes);
+    // print("IMAGEN GUARDADA: https://www.gravatar.com/avatar/${digest.toString()}?s=${widget.size}&d=$gravatarAttr");
+    return 'https://www.gravatar.com/avatar/${digest.toString()}?s=${widget.size}&d=$gravatarAttr';
+  }
+
+  Widget takeUserImage(String gravatarAttr, double sizes) {
+    String? email = FirebaseAuth.instance.currentUser?.email;
+    if (email != null) {
+      String gravatarUrl = _generateGravatarUrl(email, gravatarAttr);
+      return ClipOval(
+        child: Image.network(
+          gravatarUrl,
+          width: sizes,
+          height: sizes,
+          fit: BoxFit.cover,
+          errorBuilder: (context, error, stackTrace) {
+            return Icon(Icons.account_circle, size: sizes);
+          },
+        ),
+      );
+    } else {
+      return Icon(Icons.account_circle, size: sizes);
+    }
   }
 
   void _profileRedirection() {
-    Get.offAllNamed(Routes.home); /// Esto debe redirigir a la página de los datos del usuario
+    Get.toNamed(Routes.profile);
   }
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: _profileRedirection,
-      icon: _getUserImage(),
-      iconSize: 90,
+      onPressed: () {
+          _profileRedirection();
+      },
+      icon: takeUserImage(gravatarAttr, widget.size),
+      iconSize: widget.size + 10.0,
     );
   }
 }
+
