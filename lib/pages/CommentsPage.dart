47a6daa0ca5d3908399d7e94e9f8312c6621dddd
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-04-21 20:29:38 +0200

    CommentsPage created and AppPages and AppRoutes edited
    Added the CommentsPage to the app

---
+++ lib/pages/CommentsPage.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:game_box/components/UserCommentsListComponent.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import '../auth/structure/controllers/AuthController.dart';
+import '../components/SearchPlaceholder.dart';
+import '../components/SearchResults.dart';
+import '../components/ToolBar.dart';
+import '../components/UserImage.dart';
+import '../components/UserName.dart';
+import '../routes/AppRoutes.dart';
+
+class CommentsPage extends StatefulWidget {
+  const CommentsPage({super.key});
+
+  @override
+  State<CommentsPage> createState() => _CommentsPageState();
+}
+
+class _CommentsPageState extends State<CommentsPage> {
+
+  bool _isSidebarOpen = false;
+
+  void _toggleSidebar() {
+    setState(() {
+      _isSidebarOpen = !_isSidebarOpen;
+    });
+  }
+
+  void _closeSidebar() {
+    setState(() {
+      _isSidebarOpen = false;
+    });
+  }
+
+  AuthController _authController = AuthController();
+
+  void _signOut() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Sign Out'),
+          content: Text('¿Are you sure you want to Sign out?'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('No'),
+              onPressed: () {
+                Navigator.of(context).pop(); // It closes the dialog
+              },
+            ),
+            TextButton(
+              child: Text('Yes'),
+              onPressed: () {
+                _authController.signOut();
+                Get.offAllNamed(Routes.login);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      bottomNavigationBar: ToolBar(onMenuPressed: _toggleSidebar),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              AppBar(
+                leading: IconButton(
+                  onPressed: () {
+                    Get.back();
+                  },
+                  color: Colors.white,
+                  icon: Icon(Icons.arrow_back),
+                ),
+                title: UserImage(size: 45,),
+                actions: [
+                  SearchPlaceholder(),
+                  if (kIsWeb) UserName(),
+                  IconButton(
+                    onPressed: _signOut,
+                    icon: Icon(Icons.logout_rounded),
+                    color: Colors.white,
+                    iconSize: 30,
+                  ),
+                ],
+                backgroundColor: Colors.black,
+              ),
+
+              ///Content
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      SizedBox(height: 20,),
+
+                      UserCommentsListComponent(),
+
+
+                    ],
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+
+          /// BARRA LATERAL + OVERLAY
+          if (_isSidebarOpen)
+            GestureDetector(
+              onTap: _closeSidebar,
+              child: Container(
+                color: Colors.black54, // oscurece el fondo
+                width: double.infinity,
+                height: double.infinity,
+              ),
+            ),
+
+          AnimatedPositioned(
+            duration: Duration(milliseconds: 300),
+            left: _isSidebarOpen ? 0 : -250,
+            top: 0,
+            bottom: 0,
+            child: Container(
+              width: 250,
+              color: Colors.grey[900],
+              child: Column(
+                children: [
+                  SizedBox(height: 50),
+                  ListTile(
+                    title: Text("Inicio", style: TextStyle(color: Colors.white)),
+                    onTap: () => Get.offAllNamed(Routes.home),
+                  ),
+                  ListTile(
+                    title: Text("Comentarios", style: TextStyle(color: Colors.white)),
+                    onTap: () => Get.offAllNamed(Routes.comments),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          SearchResults(), // Muestra la lista de resultados
+        ],
+      ),
+    );
+  }
+}
+
