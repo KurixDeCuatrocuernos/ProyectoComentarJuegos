61bd5dbcf59a2f979ccd03780e84ef1f849ed8aa
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-04-21 20:41:07 +0200

    CommentaryRepository edited
    Added the methods:
     · getCommentsByGame(): to get all the comments from a game
     · countCommentsByGame(): to count how many comments has a game
     · getWeightAndValuesByGame(): to get the weight and the values that has the comments
     · getUserCommentariesByUid(): to get the commentaries from a user.

--- lib/repository/CommentaryRepository.dart
+++ lib/repository/CommentaryRepository.dart
@@ -1,6 +1,8 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:game_box/repository/UserRepository.dart';
+import 'package:get/get.dart';
 
 class CommentaryRepository {
 
@@ -82,6 +84,91 @@ class CommentaryRepository {
     } catch (error){
       print("ERROR AL OBTENER EN COMENTARIO: $error");
       return null;
+    }
+  }
+
+  Future<List<Map<String, dynamic>>?> getCommentsByGame(Map<String, dynamic> game) async{
+    try {
+      final QuerySnapshot response = await FirebaseFirestore.instance
+          .collection(_collection)
+          .where('gameId', isEqualTo: game['id'])
+          .orderBy('createdAt', descending: true)
+          .limit(10)
+          .get();
+      if(response.docs.isNotEmpty){
+        List<Map<String, dynamic>> docs = response.docs.map((doc)=>doc.data() as Map<String,dynamic>).toList();
+        UserRepository _userRepo = UserRepository();
+        for (var element in docs) {
+          String? username = await _userRepo.getUserNameByUid(element['userId']);
+          if (username != null) {
+            element['name'] = username;
+          } else {
+            element['name'] = 'Username';
+          }
+        }
+        // print("Resultados obtenidos: $docs");
+        return docs;
+      } else {
+        print("SE OBTUVO UNA LISTA DE COMENTARIOS VACÍA");
+        return null;
+      }
+    } catch (error) {
+      print("ERROR AL OBTENER LOS COMENTARIOS");
+      return null;
+    }
+  }
+
+  Future<int?> countCommentsByGame(Map<String, dynamic> game) async {
+    try{
+      final QuerySnapshot response = await FirebaseFirestore.instance
+          .collection(_collection)
+          .where('gameId', isEqualTo: game['id'])
+          .get();
+      return response.size;
+    } catch (error) {
+      print("ERROR AL OBTENER LOS COMENTARIOS");
+      return null;
+    }
+  }
+
+  Future<List<Map<String, dynamic>>?> getWeightAndValuesByGame(Map<String, dynamic> game) async {
+    try {
+      UserRepository _userRepo = UserRepository();
+      List<Map<String, dynamic>>? comments = await getCommentsByGame(game);
+      if (comments!.isNotEmpty) {
+        List<Map<String, dynamic>> retorno = [];
+        for (var comment in comments) {
+          retorno.add({
+            'value': comment['value'].toDouble(),
+            'weight': await _userRepo.getWeightById(comment['userId']),
+          });
+        }
+        return retorno;
+      } else {
+        print("La lista de comentarios está vacía");
+        return null;
+      }
+    } catch (error) {
+      print("Error al obtener los datos");
+      return null;
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getUserCommentariesByUid(String uid) async {
+    List<Map<String, dynamic>> list = [];
+    try {
+      final QuerySnapshot response = await FirebaseFirestore.instance
+          .collection(_collection)
+          .where('userId', isEqualTo: uid)
+          .orderBy('createdAt', descending: true)
+          .get();
+      if (response.docs.isNotEmpty) {
+        list = response.docs.map((doc)=>doc.data() as Map<String,dynamic>).toList();
+      }
+      return list;
+    } catch (error) {
+      print("HUBO UN ERROR AL OBTENER LOS DATOS: $error");
+      return list;
     }
   }
 
